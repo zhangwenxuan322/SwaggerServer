23c7f2b531cc2a7cf6bde60101177cffe922ed27
zhangwenxuan322 <798043150@qq.com>
2020-02-19 15:21:47 +0800

    基础架构

---
+++ src/main/java/indi/swagger/controller/UserController.java
@@ -0,0 +1,13 @@
+package indi.swagger.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @program: Swagger
+ * @description: 用户控制层
+ * @author: 张文轩
+ * @create: 2020-02-19 15:20
+ **/
+@RestController
+public class UserController {
+}
--- src/main/java/indi/swagger/service/impl/UserServiceImpl.java
+++ src/main/java/indi/swagger/service/impl/UserServiceImpl.java
@@ -8,7 +8,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-
 /**
  * @program: Swagger
  * @description: 用户服务实现类
